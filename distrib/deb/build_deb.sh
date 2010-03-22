e0f2355f659fe9388b87b0a60a60403e7fdd6922
dtsyss <dtsyss@88de09d6-77c0-11de-adf2-cbd3851e0ccf>
2010-03-22 19:00:56 +0000

    build deb amd64/32_tr
    git-svn-id: https://urtconnector.googlecode.com/svn/trunk@110 88de09d6-77c0-11de-adf2-cbd3851e0ccf

--- distrib/deb/build_deb.sh
+++ distrib/deb/build_deb.sh
@@ -3,22 +3,15 @@ a=$(pwd)
 cd ..
 cd ..
 b=$(pwd)
-sh build_gcc_linux_release
 cd $a
 mkdir -p debian
 cd debian
 mkdir -p usr
 mkdir -p DEBIAN
-cd usr
-mkdir -p local
-cd local
-mkdir bin
-cd ..
-mkdir share
-cd share
-mkdir icons
-cp $b/release/src/urtconnector $a/debian/usr/local/bin/
-cp $b/src/icons/urtconnector.png $a/debian/usr/share/icons/
+cd $b
+cmake -DCMAKE_INSTALL_PREFIX=$a/debian/usr
+make
+make install/strip
 cd $a
 c=$(uname -m)
 if [ "$c" = 'x86_64' ]; then
