e51626b9ddf915f7c2a4659e22494e6aaa0f4ec8
navrocky.vlad <navrocky.vlad@88de09d6-77c0-11de-adf2-cbd3851e0ccf>
2010-05-31 19:54:33 +0000

    fixed about dialog look on windows
    git-svn-id: https://urtconnector.googlecode.com/svn/trunk@173 88de09d6-77c0-11de-adf2-cbd3851e0ccf

--- src/about_dialog.cpp
+++ src/about_dialog.cpp
@@ -1,3 +1,5 @@
+#include <QPalette>
+
 #include "config.h"
 
 #include "ui_about_dialog.h"
@@ -19,7 +21,13 @@ about_dialog::about_dialog(QWidget *parent)
 {
     ui_->setupUi(this);
     ui_->label_version->setText(tr("version %1 \"%2\"").arg(URT_VERSION).arg(URT_CODENAME));
-    ui_->about_label->setText(prepare_html(tr(
+
+    QPalette transp_palette;
+    transp_palette.setColor(QPalette::Base, Qt::transparent);
+    transp_palette.setColor(QPalette::Text, transp_palette.color(QPalette::WindowText));
+
+    ui_->about_browser->setPalette(transp_palette);
+    ui_->about_browser->setHtml(prepare_html(tr(
         "<p>UrTConnector intended for help in everyday life of professional "
         "<a href=\"http://www.urbanterror.net\">UrbanTerror</a> player.</p>"
         "<p>Search for game servers, players, online clans, bookmarks, "
@@ -30,7 +38,8 @@ about_dialog::about_dialog(QWidget *parent)
         "<p>License: <a href=\"http://www.gnu.org/licenses/gpl.html\">"
         "GNU General Public License Version 3</a></p>"
     )));
-    ui_->developers_label->setText(prepare_html(tr(
+    ui_->developers_browser->setPalette(transp_palette);
+    ui_->developers_browser->setHtml(prepare_html(tr(
         "<p>Developed by members of the best russian clan "
         "<a href=\"http://www.urt-xaoc.ru\">=XaoC=</a>.</p>"
         "<p class=\"name\">Navrocky Vladislav (=XaoC=vlad.ru)<p>"
@@ -43,12 +52,14 @@ about_dialog::about_dialog(QWidget *parent)
         "<p class=\"info\"><a href=\"mailto:kinallru@gmail.com\">kinallru@gmail.com</a><br>"
         "Code, ideas, Gentoo packager.</p>"
     )));
-    ui_->thanks_label->setText(prepare_html(tr(
+    ui_->thanks_browser->setPalette(transp_palette);
+    ui_->thanks_browser->setHtml(prepare_html(tr(
         "<p class=\"name\">Tsyss Dmitry (=XaoC=Dmitro)<p>"
         "<p class=\"info\"><a href=\"mailto:dtsyss@gmail.com\">dtsyss@gmail.com</a><br>"
         "Clan leader, help on project, tester.</p>"
     )));
-    ui_->translate_label->setText(prepare_html(tr(
+    ui_->translation_browser->setPalette(transp_palette);
+    ui_->translation_browser->setHtml(prepare_html(tr(
         "<p>UrTConnector translated to many languages. "
         "Thanks to people who helped with translation.</p>"
         "<p>If you want help with translation UrTConnector to your language - "
