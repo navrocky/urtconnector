63ea5bd75eb59f36d3998bf4b0c6fbe952ac6fd4
navrocky.vlad <navrocky.vlad@88de09d6-77c0-11de-adf2-cbd3851e0ccf>
2010-12-08 10:47:19 +0000

    leaks fixed
    git-svn-id: https://urtconnector.googlecode.com/svn/trunk@308 88de09d6-77c0-11de-adf2-cbd3851e0ccf

--- src/common/qaccumulatingconnection.cpp
+++ src/common/qaccumulatingconnection.cpp
@@ -1,25 +1,27 @@
 #include <QTimerEvent>
- 
+
 #include "qaccumulatingconnection.h"
- 
+
 QAccumulatingConnection::QAccumulatingConnection(int emitInterval, EmitMode mode, QObject* parent)
-: emitInterval_(emitInterval)
+: QObject(parent)
+, emitInterval_(emitInterval)
 , timerId_(0)
 , emitMode_(mode)
 {
 }
- 
+
 QAccumulatingConnection::QAccumulatingConnection(const QObject* sender, const char* signal,
         const QObject* receiver, const char* slot, int emitInterval,
         EmitMode mode, QObject* parent)
-: emitInterval_(emitInterval)
+: QObject(parent)
+, emitInterval_(emitInterval)
 , timerId_(0)
 , emitMode_(mode)
 {
     QObject::connect(sender, signal, this, SLOT(emitSignal()));
     QObject::connect(this, SIGNAL(signal()), receiver, slot);
 }
- 
+
 void QAccumulatingConnection::emitSignal()
 {
     if (emitMode_ == Finally && timerId_)
@@ -31,16 +33,6 @@ void QAccumulatingConnection::emitSignal()
         timerId_ = startTimer(emitInterval_);
 }
 
-void QAccumulatingConnection::emitNow()
-{
-    if (timerId_)
-    {
-        killTimer(timerId_);
-        timerId_ = 0;
-    }
-    emit signal();
-}
- 
 void QAccumulatingConnection::timerEvent(QTimerEvent* e)
 {
     if (e->timerId() != timerId_)
@@ -48,4 +40,18 @@ void QAccumulatingConnection::timerEvent(QTimerEvent* e)
     killTimer(timerId_);
     timerId_ = 0;
     emit signal();
+}
+
+void QAccumulatingConnection::emitNow()
+{
+    resetEmit();
+    emit signal();
+}
+
+void QAccumulatingConnection::resetEmit()
+{
+    if (!timerId_)
+        return;
+    killTimer(timerId_);
+    timerId_ = 0;
 }
