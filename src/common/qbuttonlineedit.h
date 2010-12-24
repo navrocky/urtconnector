bae5fb989275d3e0a5b573d34d5f1d0c37aa4f31
navrocky.vlad <navrocky.vlad@88de09d6-77c0-11de-adf2-cbd3851e0ccf>
2010-12-24 14:54:32 +0000

    lineedit with buttons added
    git-svn-id: https://urtconnector.googlecode.com/svn/trunk@321 88de09d6-77c0-11de-adf2-cbd3851e0ccf

---
+++ src/common/qbuttonlineedit.h
@@ -0,0 +1,57 @@
+/* 
+ * File:   qbuttonlineedit.h
+ * Author: navrocky.vlad
+ */
+
+#ifndef QBUTTONLINEEDIT_H
+#define	QBUTTONLINEEDIT_H
+
+#include <QLineEdit>
+#include <QToolButton>
+#include <QList>
+#include <QMap>
+
+class QButtonLineEdit : public QLineEdit
+{
+    Q_OBJECT
+public:
+    enum Position
+    {
+        Left,
+        Right
+    };
+
+    QButtonLineEdit(QWidget* parent = NULL);
+
+    void addWidget(QWidget* w, Position pos = Right);
+    void insertWidget(int index_before, QWidget* w, Position pos = Right);
+    void removeWidget(QWidget* w);
+    QWidget* takeWidget(QWidget* w);
+
+    QToolButton* addButton(Position pos = Right);
+    QToolButton* addActionButton(QAction* a, Position pos = Right);
+
+    virtual bool event(QEvent* e) OVERRIDE;
+
+protected:
+    virtual QSize minimumSizeHint() const;
+    virtual void resizeEvent(QResizeEvent* e);
+    
+private slots:
+    void widgetDeleted(QObject*);
+
+private:
+    int calcWidgetsWidth(Position pos) const;
+    int calcMaxHeight(Position pos) const;
+    QSize widgetSize(QWidget* w) const;
+    void arrangeWidgets();
+    QWidget* takeWidgetInt(QWidget* w);
+
+    typedef QList<QWidget*> Widgets;
+    typedef QMap<Position, Widgets> AllWidgets;
+
+    AllWidgets m_widgets;
+    int m_space;
+};
+
+#endif	/* QBUTTONLINEEDIT_H */
