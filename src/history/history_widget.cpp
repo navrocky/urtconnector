fd00780b1a940a4914ed2c535cd6f017d7dc8365
kinnalru <kinnalru@88de09d6-77c0-11de-adf2-cbd3851e0ccf>
2011-03-10 21:44:28 +0000

    playing with SFINAE in history widget
    git-svn-id: https://urtconnector.googlecode.com/svn/trunk@367 88de09d6-77c0-11de-adf2-cbd3851e0ccf

--- src/history/history_widget.cpp
+++ src/history/history_widget.cpp
@@ -6,6 +6,7 @@
 #include <algorithm>
 #include <boost/bind.hpp>
 #include <boost/utility/enable_if.hpp>
+#include <boost/iterator/iterator_facade.hpp>
 
 #include <common/qt_syslog.h>
 #include <common/server_list.h>
@@ -154,23 +155,20 @@ struct std_adapter
     }
 };
 
-template <typename T>
-struct class_has_find{
 
-    typedef char yes_type;
-    struct no_type { yes_type dummy[2]; };
+//metaclass for checking if class has find functionm with Arg argument and Ret result type
+template<typename T, typename Ret, typename Arg> struct class_has_find
+{
+    typedef char 	yes_type;
+    typedef int 	no_type;
+    
+    template<typename U, Ret (U::*fn)(Arg) const> struct Checker
+    { typedef yes_type result; };
 
-    template <void (T::*)()> struct void_tester;
+    template<typename U> static typename Checker<U, &U::find>::result has_member(U*);
+    static no_type has_member(...);
 
-    template <typename U>
-    static yes_type has_member( void_tester<&U::find>* );
-
-    template <typename U>
-    static no_type has_member( ... );
-
-    //static const bool value = sizeof( has_member<T>(0) ) == sizeof(no_type);
-    static const bool value = sizeof( has_member<T>(0) ) == sizeof(yes_type);
-
+    static const int value = sizeof(has_member((T*)NULL)) == 1 ? 1 : 0;      
 };
 
 template <typename T>
@@ -183,18 +181,13 @@ struct get_value_type< QMap<T, U> >{
     typedef U type;
 };
 
-template <typename T, typename U>
-struct get_value_type< std::map<T, U> >{
-    typedef U type;
-};
-
-template <typename List, typename Adapter = std_adapter>
+template <typename Container, typename Adapter = std_adapter>
 struct updater
 {
-    typedef typename get_value_type<List>::type Element;
+    typedef typename get_value_type<Container>::type Element;
     typedef QMap<Element, QTreeWidgetItem*> item_map_t;
 
-    static void update_tree_contents(const List& l,
+    static void update_tree_contents(const Container& l,
                               int role,
                               QTreeWidget* tree,
                               QTreeWidgetItem* parent_item,
@@ -222,7 +215,8 @@ struct updater
         typename item_map_t::iterator it = items.begin();
         for (; it != items.end(); ++it)
         {
-           typename List::const_iterator i = find(l, it.key());
+//            typename List::const_iterator i =
+           find(l, it.key());
 //            std::find( l.begin(), l.end(),  )
 //            l.find(it.key());
         }
@@ -233,23 +227,90 @@ struct updater
 
     }
 
+
+    // using SFINAE for handling different types of container 
+    // http://en.wikipedia.org/wiki/Substitution_failure_is_not_an_error
+
     template <typename T>
-    static typename List::const_iterator find(
-        const T& l, const Element& v,
-        typename boost::disable_if<class_has_find<T> >::type* dummy = 0
+    static typename Container::const_iterator find(
+        const T& container, const Element& element,
+        typename boost::disable_if< class_has_find<T, typename Container::const_iterator, const Element&> >::type* dummy = 0
     )
     {
-        return std::find(l.begin(), l.end(), v);
+        return std::find(container.begin(), container.end(), element);
     }
 
     template <typename T>
-    static typename List::const_iterator find(
-        const T& l, const Element& v,
-        typename boost::enable_if<class_has_find<T> >::type* dummy = 0
+    static typename Container::const_iterator find(
+        const T& container, const Element& element,
+        typename boost::enable_if<class_has_find<T, typename Container::const_iterator, const Element&> >::type* dummy = 0
         )
     {
-        return l.find(v);
+        return container.find(element);
     }
+};
+
+
+
+//struct that adapts std::map tp QMap type interfaces. class holds only refference to the std::map
+template <typename Map>
+struct map_adapter{
+    typedef typename Map::key_type key_type;
+    typedef typename Map::mapped_type mapped_type;
+    
+    typedef typename Map::mapped_type value_type;
+    
+    typedef std::map<key_type, mapped_type> RestoredMap;
+    
+    RestoredMap& map;
+    
+    map_adapter(Map& m):map(m){}
+    
+    template <class Type, class BaseIterator>
+    class adapt_iterator
+      : public boost::iterator_facade<
+	    adapt_iterator<Type, BaseIterator>
+	  , Type
+	  , boost::forward_traversal_tag
+	>
+    {
+      public:
+	  adapt_iterator() : it() {}
+
+	  explicit adapt_iterator(BaseIterator it) : it(it) {}
+
+	  template <class T,class I>
+	  adapt_iterator(adapt_iterator<T, I> const& other) : it(other.it) {}
+
+      private:
+	  friend class boost::iterator_core_access;
+	  template <class, class> friend class adapt_iterator;
+
+	  template <class T,class I>
+	  bool equal(adapt_iterator<T, I> const& other) const
+	  { return this->it == other.it; }
+
+	  void increment()
+	  { ++it; }
+
+	  Type& dereference() const
+	  { return it->second; }
+
+	  BaseIterator it;
+    };
+    
+    typedef adapt_iterator<value_type, typename RestoredMap::iterator > iterator;
+    typedef adapt_iterator<value_type const, typename RestoredMap::const_iterator > const_iterator;
+    
+    iterator begin() { return iterator(map.begin()); }
+    const_iterator begin() const { return const_iterator(map.begin()); }
+    
+    iterator end() { return iterator(map.end()); }
+    const_iterator end() const { return const_iterator(map.end()); }
+    
+    iterator find( const key_type& k) { return iterator(map.find(k)); }
+    const_iterator find( const key_type& k) const { return const_iterator(map.find(k)); }
+    
 };
 
 void history_widget::update_item(QTreeWidgetItem*)
@@ -264,18 +325,24 @@ void history_widget::update_contents_simple()
 //    QMap<int, int>::
 //    history::history_list_t::value_type
 //    std_adapter a;
-
+// 
     updater<history::history_list_t>::update_tree_contents(hl, c_history_role, tree_, 0,
         boost::bind(&history_widget::update_item, this, _1), items_map_);
 
-    typedef QMap<int, int> my_type;
+//     typedef QMap<int, int> my_type;
+    typedef std::map<int, int> my_type;
     typedef QMap<int, QTreeWidgetItem*> my_items;
 
     my_type l;
     my_items items;
 
+    typedef map_adapter<my_type> MT;
+    MT mt(l);
 
-    updater<my_type>::update_tree_contents(l, c_history_role, tree_, 0,
+//     updater<my_type>::update_tree_contents(l, c_history_role, tree_, 0,
+//         boost::bind(&history_widget::update_item, this, _1), items);
+    
+    updater<MT>::update_tree_contents(mt, c_history_role, tree_, 0,
         boost::bind(&history_widget::update_item, this, _1), items);
 
 
