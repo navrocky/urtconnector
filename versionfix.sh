e735f066cee30982facc99fc7215e9f52d820b70
pztrn@pztrn.ru <pztrn@pztrn.ru@88de09d6-77c0-11de-adf2-cbd3851e0ccf>
2011-07-10 04:30:11 +0000

    I hate regexp >.<
    git-svn-id: https://urtconnector.googlecode.com/svn/trunk@481 88de09d6-77c0-11de-adf2-cbd3851e0ccf

--- versionfix.sh
+++ versionfix.sh
@@ -5,7 +5,7 @@ REV=`svnversion`
 #
 winfix() {
   sed s/"0.7.1 build ..."/"0.7.1 build $REV"/g distrib/windows/urtconnector.nsi > distrib/windows/urtconnector.nsi.1
-  sed s/"0.7.1 build ..."/"0.7.1 build $REV"/g version > version.1
+  sed s/"0.7.1 build ...."/"0.7.1 build $REV"\"\)/g version > version.1
   rm distrib/windows/urtconnector.nsi version
   mv distrib/windows/urtconnector.nsi.1 distrib/windows/urtconnector.nsi
   mv version.1 version
@@ -13,7 +13,7 @@ winfix() {
 }
 
 linfix() {
-  sed s/"0.7.1 build ..."/"0.7.1 build $REV"/g version > version.1
+  sed s/"0.7.1 build .*"/"0.7.1 build $REV"\"\)/g version > version.1
   rm version
   mv version.1 version
   echo "! LINUX VERSION FIXED"
