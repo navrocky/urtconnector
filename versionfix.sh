6215daf9cf8c4c85743735b5139419ff5863bc41
pztrn@pztrn.ru <pztrn@pztrn.ru@88de09d6-77c0-11de-adf2-cbd3851e0ccf>
2011-07-10 04:56:56 +0000

    Yep, I really need to grab a cup of coffee... Or to do a suicide, I don't know :<
    git-svn-id: https://urtconnector.googlecode.com/svn/trunk@483 88de09d6-77c0-11de-adf2-cbd3851e0ccf

--- versionfix.sh
+++ versionfix.sh
@@ -4,9 +4,9 @@
 REV=`svnversion`
 #
 winfix() {
-  sed s/"0.7.1 build ..."/"0.7.1 build $REV"/g distrib/windows/urtconnector.nsi > distrib/windows/urtconnector.nsi.1 || echo "Failed to nodify NSIS script" && exit 1
-  sed s/"0.7.1 build .*"/"0.7.1 build $REV"\"\)/g version > version.1 || echo "Failed to modify VERSION file" && exit 1
-  rm distrib/windows/urtconnector.nsi version || echo "Failed to remove old NSIS script or version file" && exit 1
+  sed s/"0.7.1 build .*"/"0.7.1 build $REV"/g distrib/windows/urtconnector.nsi > distrib/windows/urtconnector.nsi.1 || echo "Failed to nodify NSIS script" && exit 1
+  sed s/"0.7.1 build .*"/"0.7.1 build $REV"\"\)/g version > version.1 || echo "Failed to modify VERSION file"
+  rm distrib/windows/urtconnector.nsi version || echo "Failed to remove old NSIS script or version file"
   mv distrib/windows/urtconnector.nsi.1 distrib/windows/urtconnector.nsi || echo "Failed to move new NSIS script"
   mv version.1 version || echo "Failed to move new VERSION file"
   echo "! WINDOWS VERSION FIXED"
