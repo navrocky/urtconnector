444a52c1095f2511c2c31edcd8904b8fb439212a
pztrn@pztrn.ru <pztrn@pztrn.ru@88de09d6-77c0-11de-adf2-cbd3851e0ccf>
2011-07-10 05:10:35 +0000

    Coffee grabbed. All works for now...
    git-svn-id: https://urtconnector.googlecode.com/svn/trunk@484 88de09d6-77c0-11de-adf2-cbd3851e0ccf

--- versionfix.sh
+++ versionfix.sh
@@ -4,7 +4,7 @@
 REV=`svnversion`
 #
 winfix() {
-  sed s/"0.7.1 build .*"/"0.7.1 build $REV"/g distrib/windows/urtconnector.nsi > distrib/windows/urtconnector.nsi.1 || echo "Failed to nodify NSIS script" && exit 1
+  sed s/"0.7.1 build .*"/"0.7.1 build $REV"\"/g distrib/windows/urtconnector.nsi > distrib/windows/urtconnector.nsi.1 || echo "Failed to nodify NSIS script"
   sed s/"0.7.1 build .*"/"0.7.1 build $REV"\"\)/g version > version.1 || echo "Failed to modify VERSION file"
   rm distrib/windows/urtconnector.nsi version || echo "Failed to remove old NSIS script or version file"
   mv distrib/windows/urtconnector.nsi.1 distrib/windows/urtconnector.nsi || echo "Failed to move new NSIS script"
